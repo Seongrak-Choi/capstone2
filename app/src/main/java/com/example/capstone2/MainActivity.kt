bbf60e2449961a9a49ad70b5c4cd1103280916e4
Seongrak-CHOI <godgod153@gmail.com>
2021-04-07 22:00:02 +0900

    최성락 git 연동 테스트 중입니다.

--- app/src/main/java/com/example/capstone2/MainActivity.kt
+++ app/src/main/java/com/example/capstone2/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //최성락 안드로이드 스튜디오에서 git연동 테스트 중입니다.
     }
 }
